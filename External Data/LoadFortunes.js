cced2c7409589b298aa4d0ece53e2c9969698df1
Andrews-repo <59451276+Andrews-repo@users.noreply.github.com>
2020-04-05 21:48:51 -0700

    maybe

---
+++ External Data/LoadFortunes.js
@@ -0,0 +1,30 @@
+var AWS = require("aws-sdk");
+var fs = require('fs');
+
+AWS.config.update({
+    region: "us-east-1",
+    endpoint: "https://dynamodb.us-east-1.amazonaws.com"
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+console.log("Importing Fortunes into DynamoDB. Please wait.");
+
+var allFortunes = JSON.parse(fs.readFileSync('Fortunesdata.json', 'utf8'));
+allFortunes.forEach(function(Fortune) {
+    var params = {
+        TableName: "Fortunes",
+        Item: {
+            "FortuneId: Fortunes.FortuneId,
+            "Fortune": Fortunes.Fortune,
+        }
+    };
+
+    docClient.put(params, function(err, data) {
+       if (err) {
+           console.error("Unable to add Fortune", Fortunes.Fortune, ". Error JSON:", JSON.stringify(err, null, 2));
+       } else {
+           console.log("PutItem succeeded:", Fortunes.Fortune);
+       }
+    });
+});
